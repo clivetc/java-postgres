1a4aa9025e4b624df9baf83a55a30cb2f062cbbb
clivetc <clivetchikwape@gmail.com>
2023-12-17 19:45:29 +0200

    first commit

---
+++ src/main/java/com/example/demo/Student/StudentConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.Student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+
+                    new Student(1L,"John","example@example.com", LocalDate.of(2000, Month.JULY,18));
+
+        };
+    }
+
+}
